e08ea56c08108a706b1f5d24ee9aaf6ce056156b
Meghann Stamper <meg.stamp@yahoo.com>
2023-05-09 14:40:22 -0400

    initial checkin

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+// Console.WriteLine("Hello, World!");
+
+
+int age = 26;
+string name = "Meghann";
+bool isHere = true;
+double salary = 22000.01;
+
+
+Console.WriteLine($"Hi, {name}. Your age is {age} and is here? {isHere}");
+Console.WriteLine($"And you make ${salary} a year");
